8e8acc30723d25a7215e1064293c431be736dac7
Thayana Mamoré <thayanacmamore@gmail.com>
2022-02-17 20:38:20 -0300

    ✨ MindAR Vue parent and child component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,54 @@
+<script setup>
+  import 'mind-ar/dist/mindar-image.prod.js'
+  import 'aframe'
+  import 'mind-ar/dist/mindar-image-aframe.prod.js'
+  import MindarViewer from './components/MindarViewer.vue'
+  import { ref } from 'vue'
+
+  const mindarViewerRef = ref(null)
+  const title = ref('START')
+
+  function handleARSystems() {    
+    const sceneEl = mindarViewerRef.value.sceneRef
+    const arSystem = sceneEl.systems['mindar-image-system']
+    if(title.value == 'START') {
+      arSystem.start()
+      title.value = 'STOP'
+    } else {
+      arSystem.stop()
+      title.value = 'START'
+    }
+
+  }
+</script>
+
 <template>
+  <div class="app">
+    <h1>
+      Example Vue 3 with
+      <a href="https://github.com/hiukim/mind-ar-js" target="_blank">MindAR</a>
+    </h1>
+
+    <div>
+      <button @click="handleARSystems()">{{ title }}</button>
+    </div>
+
+    <div class="container">
+      <mindar-viewer ref="mindarViewerRef" />
+    </div>
+  </div>
 </template>
 
-<script>
-</script>
+<style scoped>
+.app {
+  text-align: center;
+}
+
+.container {
+  margin: 20px auto;
+  position: relative;
+  height: 400px;
+  width: 600px;
+  overflow: hidden;
+}
+</style>
---
+++ src/components/MindarViewer.vue
@@ -0,0 +1,50 @@
+<script setup>  
+  import { ref, defineExpose } from 'vue'
+
+  const sceneRef = ref(null)
+  defineExpose({ sceneRef })
+</script>
+
+<template>
+  <a-scene
+    ref="sceneRef"
+    mindar-image="imageTargetSrc: https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.0.0/examples/image-tracking/assets/card-example/card.mind; autoStart: false; uiLoading: no; uiError: no; uiScanning: no;"
+    color-space="sRGB"
+    embedded
+    renderer="colorManagement: true, physicallyCorrectLights"
+    vr-mode-ui="enabled: false"
+    device-orientation-permission-ui="enabled: false"
+  >
+    <a-assets>
+      <img
+        id="card"
+        src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.0.0/examples/image-tracking/assets/card-example/card.png"
+        crossorigin="anonymous"
+      />
+      <a-asset-item
+        id="avatarModel"
+        src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.0.0/examples/image-tracking/assets/card-example/softmind/scene.gltf"
+        crossorigin
+      ></a-asset-item>
+    </a-assets>
+
+    <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
+
+    <a-entity mindar-image-target="targetIndex: 0">
+      <a-plane
+        src="#card"
+        position="0 0 0"
+        height="0.552"
+        width="1"
+        rotation="0 0 0"
+      ></a-plane>
+      <a-gltf-model
+        rotation="0 0 0 "
+        position="0 0 0.1"
+        scale="0.005 0.005 0.005"
+        src="#avatarModel"
+        animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate"
+      ></a-gltf-model>
+    </a-entity>
+  </a-scene>
+</template>
